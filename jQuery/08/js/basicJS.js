5b3877eebdcffa76a4f539eab872be95c7920eac
Sanket4496 <37980063+Sanket4496@users.noreply.github.com>
2020-06-30 14:10:48 +0530

    Add files via upload

---
+++ jQuery/08/js/basicJS.js
@@ -0,0 +1,15 @@
+ $(function(){
+   $(".btn1").click(function(){
+      $("div").addClass("new");  
+   }); 
+});
+
+//removeClass()
+
+
+$(function(){
+   $(".btn2").click(function(){
+      $("div").toggleClass("new");  
+   }); 
+});
+
